00d0d852a1f2ee85dffaf64e4f93d68a4fd0517a
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2018-06-27 16:30:27 +0530

    syntax/variables added

---
+++ Syntax/variables.py
@@ -0,0 +1,17 @@
+#variables
+#you dont need to declare data type in python while creating a variable
+
+variable_name = "this is how you assign a variable"
+can_declare_variable_as_integer = 55
+can_declare_variable_as_float = 55.55
+
+can_declare_variable_as_float1 = 55e2 #eindicates power of 10
+print(can_declare_variable_as_float1)
+
+multiline_variable = """this is how,
+you declare
+a multi line 
+variable, it
+can be used to
+write an address"""
+print(multiline_variable)
