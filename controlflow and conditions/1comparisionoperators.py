596cd89a6cc80ce9d682c4feb03e4ddc0a7f37b6
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2018-06-27 19:34:44 +0530

    cc/comparision operators

---
+++ controlflow and conditions/1comparisionoperators.py
@@ -0,0 +1,32 @@
+#comparators
+
+#assignment operator =
+x = 15
+print(x)
+#output : 15
+
+#equal to ==
+print(10 == 10)
+#output: True
+print(10 == 9)
+#output:False
+
+#not equal !=
+print(10 != 10)
+#output:False
+
+#less than <
+print(10 < 9)
+#output:False
+
+#greater than >
+print(10 > 9)
+#output: True
+
+#less than equal to <=
+print(10 <= 9)
+#output: False
+
+#greater than equal to >=
+print(10 >= 0)
+#output: True
