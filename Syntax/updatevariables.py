cd5a415cd95bac3a77fe3d5abe73575f687a4c94
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2018-06-27 16:33:32 +0530

    syntax/update variables added

---
+++ Syntax/updatevariables.py
@@ -0,0 +1,13 @@
+
+#updating variable values
+
+declare_new = 55
+add_value_to_new = 45
+declare_new += add_value_to_new
+print(declare_new)
+
+declare_new = 55
+subtract_value_from_new = 5
+declare_new -= subtract_value_from_new
+print(declare_new)
+
