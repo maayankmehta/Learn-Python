780abad27bf83f4b4269d8d3e249d1bccf09807e
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2018-06-27 18:21:08 +0530

    strings/methods

---
+++ Strings/3stringmethod.py
@@ -0,0 +1,18 @@
+#string methods
+
+#len() finds length of string
+print(len(find_second_letter1))
+#output: 11
+
+#lower() converts to lower case
+print(find_second_letter1.lower())
+#output: hello hello
+
+#upper() converst to caps
+print(find_second_letter1.upper())
+#output: HELLO HELLO
+
+#str() converts non string to string
+age = 22
+print(str(age))
+#output: 22
