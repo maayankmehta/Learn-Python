4f6e84ed2dc12e3ce770bdbfd82b7afa842fb977
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2018-06-27 18:21:31 +0530

    strings/strings and variables

---
+++ Strings/4stringsandvariables.py
@@ -0,0 +1,9 @@
+#print variables along with strings
+age = 22
+fav_colour = "black"
+
+#using string conversation
+print("I am " +str(age)+ " years old.")
+
+#using % operator
+print("My favourite colour is %s and I am %s years old."%(fav_colour,age))
