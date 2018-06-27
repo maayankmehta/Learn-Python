3ae6a4171d9ac9ad02321ce3ae67cdaebaac5893
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2018-06-27 16:43:59 +0530

    syntax/ValueError added

---
+++ Syntax/datatype.py
@@ -0,0 +1,11 @@
+#datatype assignment & Value Error
+
+apples = 10
+#the variable age is automatically set to an integer datatype
+#print("I have " +apples+ " apples in my bag.")
+#this generates an error as apples variable should be a string and not integer
+#convert datatype from integer to string as follows
+
+print("I have " +str(apples)+ " apples in my bag.")
+
+#other convertions are also possible like int(apples) & float(apples)
