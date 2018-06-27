aa8dc88958efdcecdbc1c099ee55b20735c5c4f6
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2018-06-27 19:54:53 +0530

    syntax/take user input

---
+++ Syntax/7input.py
@@ -0,0 +1,8 @@
+
+#input from user
+print("Choose anyone")
+print("1 for Rock, 2 for Paper, 3 for Scissors")
+take_input = input("Enter your choice: ")
+print(take_input)
+
+#python2 raw_input() was renamed to input() in python3
