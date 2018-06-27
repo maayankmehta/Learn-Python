d03a74139d07880776715bf5b09289221d7c0d15
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2018-06-27 18:20:47 +0530

    strings/access methods

---
+++ Strings/2accessstring.py
@@ -0,0 +1,7 @@
+#access string using index
+
+find_second_letter = "HELLO"[4]
+find_second_letter1 ="HELLO hello"
+
+print (find_second_letter)
+#this prints the letter at position 4, starts from 0 i.e 'O'
