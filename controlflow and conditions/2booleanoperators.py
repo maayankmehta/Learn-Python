51259f2d5c8f3a49922f57d83b29adaf15f9bf5b
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2018-06-27 19:35:07 +0530

    cc/boolean operators

---
+++ controlflow and conditions/2booleanoperators.py
@@ -0,0 +1,14 @@
+#boolean operators
+#order of operations NOT->AND->OR
+
+#AND
+print(2 == 2 and 2 != 1)
+#output: True and True = True
+
+#OR
+print(2 == 2 or 2 != 2)
+#output: True or False = True
+
+#NOT
+print(not True)
+#output: not True = False
