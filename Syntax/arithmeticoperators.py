f7615a10db46d6b0441e860bc13db6dbe520ff5c
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2018-06-27 16:32:01 +0530

    syntax/arithmetic operators added

---
+++ Syntax/arithmeticoperators.py
@@ -0,0 +1,33 @@
+#Arithmetic Operations
+#modulus operator % returns remainder
+#arithmetic operators in python follows BODMAS
+
+
+addition_operator = 5 + 6
+subtraction_operation = 16 - 15
+multiplication_operator = 55 * 65
+ramainder_modulus = 133 % 5
+division_quotient = 133 / 7 
+bodmas_combination = 133 + 45 / 57 - 44 * 25 % 4
+
+#use print command to print the variables
+print(bodmas_combination)
+print(addition_operator)
+print(subtraction_operation)
+print(multiplication_operator)
+print(ramainder_modulus)
+print(division_quotient)
+
+#the operators can also be directly used in the print statement as follows
+print(5 + 6)
+
+#more about division(quotient operator)
+
+number_of_chocolates = 15
+number_of_persons_share = 4
+
+integer_value = number_of_chocolates / number_of_persons_share
+print(int(integer_value)) #if you use python 2 you get an integer value as quotient
+
+float_value = float(number_of_chocolates)/number_of_persons_share
+print(float_value) #here the value obtained is a float i.e decimal
