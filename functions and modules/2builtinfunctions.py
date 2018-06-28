7d7ae2c930a11914399382408c77bb57c3f477ff
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2018-06-28 12:24:14 +0530

    functions/use builtin functions added

---
+++ functions and modules/2builtinfunctions.py
@@ -0,0 +1,19 @@
+#calling some built in functions
+
+print(max(5,4,3,2,1)) #finds the maximum out of all
+#output: 5
+
+print(min(5,4,3,2,1)) #finds the minimum out of all
+#output: 1
+
+print(abs(-500)) #finds the absolute value
+#output: 500
+
+print(type(5)) #finds the data type of the argument passed
+#output: <class 'int'>
+
+print(type(5.05))
+#output: <class 'float'>
+
+print(type('abcd'))
+#output: <class 'str'>
