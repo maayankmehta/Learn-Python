418e2b7d84fc2940db73bf925c23030075a62c4c
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2018-06-28 12:23:37 +0530

    functions/defineandcall function added

---
+++ functions and modules/1definefunction.py
@@ -0,0 +1,10 @@
+#define a function
+
+def function_name(parameter_to_be_passed):
+	"""comment"""
+	print("print something when function is called")
+	use_the_passed_parameter = parameter_to_be_passed
+	print(use_the_passed_parameter)
+
+#call the function	
+function_name(5)	#pass argument
